6a475fac75f4e8f3d0d3196f6b12ed60e407ce3d
형근 주 <lovegeun7274@gmail.com>
2020-12-08 17:26:09 +0900

    첫 커밋

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,6 @@ namespace AirForce_Sound_Supporter
         {
             if (e.Button == MouseButtons.Left)
             {
-                //현재 마우스 좌표를 저장한다.
                 mMyPoint.X = e.X;
                 mMyPoint.Y = e.Y;
             }
@@ -27,7 +26,6 @@ namespace AirForce_Sound_Supporter
         {
             if (e.Button == MouseButtons.Left)
             {
-                //현재 마우스 좌표와 저장된 마우스 좌표의 차이만큼 이동 시킨다.
                 this.Location = new Point(this.Location.X + (e.X - mMyPoint.X)
 
                 , this.Location.Y + (e.Y - mMyPoint.Y));
